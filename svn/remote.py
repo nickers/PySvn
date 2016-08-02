61bbc0adbcb71439c3d4369cda7dbbd8aa49b3c1
Tomasz Wsuł <tomasz.wsul@rightsoft.pl>
2016-08-02 11:44:14 +0200

    RemoteClient::mkdir added

--- svn/remote.py
+++ svn/remote.py
@@ -23,5 +23,17 @@ class RemoteClient(svn.common.CommonClient):
 
         self.run_command('checkout', cmd, return_binary=True)
 
+    def mkdir(self, path, message=None, parents=False):
+        cmd = []
+        if parents:
+            cmd += ['--parents']
+        cmd += [self.url + '/' + path]
+        cmd += ['-m', message if message is not None else 'created directory']
+        try:
+            self.run_command('mkdir', cmd)
+            return True
+        except ValueError as e:
+            return False
+
     def __repr__(self):
         return ('<SVN(REMOTE) %s>' % (self.url))
