3af8378ba35e105f2a097cfd2d5f8fe69bab3d03
Tomasz Wsuł <tomasz.wsul@rightsoft.pl>
2016-08-02 10:46:13 +0200

    added rm & status

--- svn/local.py
+++ svn/local.py
@@ -1,6 +1,8 @@
+import collections
 import os.path
 import svn.common
 import svn.constants
+import xml.etree.ElementTree
 
 
 class LocalClient(svn.common.CommonClient):
@@ -28,6 +30,20 @@ class LocalClient(svn.common.CommonClient):
         cmd += ['^' + repo_path, self.path]
 
         self.run_command('switch', cmd)
+
+    def rm(self, path):
+        return self.run_command('rm', [path])
+
+    def status(self, path=None):
+        if path is not None:
+            path = os.path.join(self.path, path)
+        else:
+            path = self.path
+        xmlRes = self.run_command('status', ['--xml', path])
+        root = xml.etree.ElementTree.fromstring(xmlRes)
+        se = collections.namedtuple('StatusEntry', ['path', 'status'])
+        return [se(path=e.attrib['path'], status=e.find('wc-status').attrib['item']) for e in root.findall('target/entry')]
+
 
     def __repr__(self):
         return ('<SVN(LOCAL) %s>' % (self.path))
